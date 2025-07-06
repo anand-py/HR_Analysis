4900fa6207e34700b4a7f8ba305ef88f0a809dd3
Anand Varama <84581974+anand-py@users.noreply.github.com>
2025-07-06 16:36:56 +0530

    Add files via upload

---
+++ HR_Analysis.sql
@@ -0,0 +1,31 @@
+SELECT * FROM hr_analytics.employees;
+SET SQL_SAFE_UPDATES = 0;
+UPDATE employees
+SET is_active = FALSE
+WHERE is_active IS NULL;
+-- employees earn more than $70,000
+
+SELECT full_name, salary FROM employees WHERE salary > 70000 ;
+
+-- top 5 highest-paid employees
+SELECT full_name, salary FROM employees ORDER BY salary DESC LIMIT 5;
+
+--  average salary by department?
+SELECT * FROM departments;
+SELECT department ,ROUND(AVG(salary),2) AS avg_salary FROM employees GROUP BY department  ORDER BY avg_salary DESC;
+
+-- total salary
+
+SELECT SUM(salary) AS total_salary FROM employees;
+
+-- employees are in each 
+SELECT * FROM employees;
+
+SELECT department, COUNT(*) AS total_employee FROM employees
+GROUP BY department ORDER BY total_employee DESC;
+
+--  employees have left the company?
+SELECT COUNT(*) AS departed_employees FROM employees WHERE is_active = 0;
+
+SELECT * FROM employees;
+
